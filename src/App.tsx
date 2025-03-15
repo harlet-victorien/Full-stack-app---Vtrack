cb57d35445ce66780acda117acd6d19441e604e5
vee <harlet.victorien@gmail.com>
2025-03-15 21:39:37 +0100

    fixed sizes

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
   }
 
   return (
-    <div className="flex min-h-screen bg-primary text-white">
+    <div className="flex min-h-screen bg-dark text-white">
       {/* Sidebar */}
       <div className="md:w-1/6 h-16 md:h-screen w-screen md:top-0 bottom-0 bg-darker flex md:flex-col flex-row fixed items-start md:items-center justify-center md:border-r md:border-t-0 border-t md:border-r-white/20 border-t-white/20 sm:rounded-t-lg md:rounded-r-lg md:rounded-l-none z-20">
       <nav className="md:space-y-4 md:items-center justify-start flex md:flex-col flex-row md:mb-auto md:pt-20"> 
@@ -82,7 +82,7 @@ function App() {
       </div>
   
       {/* Main Content */}
-      <div className="flex-1 md:ml-1/6 items-center">
+      <div className="flex-1 md:ml-1/6 items-center bg-dark">
         <h1
           className="md:text-5xl text-3xl font-bold mb-1 pt-10 text-center"
           style={{
